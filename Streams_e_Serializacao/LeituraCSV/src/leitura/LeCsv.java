e0892ec44f5d8b8225aa8448d120c3f300d94019
br4vier <rafaelxavierbraga@gmail.com>
2021-06-25 22:33:46 -0300

    Resolução Exercicio Lecsv

--- Streams_e_Serializacao/LeituraCSV/src/leitura/LeCsv.java
+++ Streams_e_Serializacao/LeituraCSV/src/leitura/LeCsv.java
@@ -1,6 +1,7 @@
 
 package leitura;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,21 +9,38 @@ import java.util.LinkedList;
 
 public class LeCsv {
         
-     public LinkedList<Cliente> leCsvClientes() throws IOException{         
+     private String[] split;
+
+    public LinkedList<Cliente> leCsvClientes() throws IOException{         
       
          LinkedList<Cliente> clientes = new  LinkedList<Cliente>();             
         String current = new java.io.File( "." ).getCanonicalPath();        
         File inputFile = new File(current + "\\src\\arquivo.csv");                
         FileReader in = new FileReader(inputFile);
-        int c;
-        while ((c = in.read()) != -1) {
-            c = in.read();            
-        }       
+        
+        BufferedReader b = new BufferedReader(in);
+
+        String linha;
+        linha = b.readLine();
+        linha = b.readLine();
+        
+        while(linha != null){
+            Cliente cliente = new Cliente();
+            
+            String[] vetor = linha.split(";");
+            
+            cliente.setId(vetor[0]);
+            cliente.setNome(vetor[1]);
+            cliente.setEmail(vetor[2]);
+            cliente.setTelefone(vetor[4]);
+            cliente.setTotalCompras(Double.parseDouble(vetor[5]));
+            
+            clientes.add(cliente);
+            linha = b.readLine();
+        }
+
+
         in.close(); 
-        
-        //complete o codigo para ler o csv e criar as instancias de cliente.
-        //rode o teste MainReadCsvTest.java para testar o seu código.
-        
         return clientes;
      }
 }
